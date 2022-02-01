a3e9255092c56dcdc5360b2f30a7548f511e682b
Daniel Marin <danielarturomt@gmail.com>
2022-02-01 17:44:30 +0000

    refactor: use rxjs/webSocket for rpc connection

--- apps/web-client/src/app/app.component.ts
+++ apps/web-client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { WalletName, WalletReadyState } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { Keypair, PublicKey } from '@solana/web3.js';
-import { concatMap, filter, map, of, startWith, switchMap, tap } from 'rxjs';
+import { concatMap, map, of, startWith, switchMap } from 'rxjs';
 import { SolanaRpcApiService } from './solana-rpc-api.service';
 import { SolanaRpcSocketService } from './solana-rpc-socket.service';
 import { SystemProgramApiService } from './system-program-api.service';
@@ -12,7 +12,12 @@ import { SystemProgramApiService } from './system-program-api.service';
   selector: 'transaction-processor-root',
   template: `
     <header>
-      <h1>Transaction processor</h1>
+      <h1>
+        Transaction processor
+        <ng-container *ngIf="rpcConnected$ | async">
+          (RPC CONNECTED)
+        </ng-container>
+      </h1>
 
       <select
         [ngModel]="walletName$ | async"
@@ -37,10 +42,6 @@ import { SystemProgramApiService } from './system-program-api.service';
               {{ walletPublicKey.toBase58() }} (LAMPORTS:
               {{ walletAccountLamports }})
             </p>
-
-            <button (click)="onAccountUnsubscribe(walletPublicKey)">
-              Unsubscribe
-            </button>
           </ng-container>
         </ng-container>
       </ng-container>
@@ -87,21 +88,10 @@ export class AppComponent implements OnInit {
       return this._solanaRpcApiService
         .getAccountInfo(publicKey.toBase58())
         .pipe(
-          tap((a) => console.log('da f¿?', a)),
           switchMap((accountInfo) =>
-            this._solanaRpcSocketService.accountChange$.pipe(
-              tap((a) =>
-                console.log(
-                  a,
-                  a.pubkey,
-                  publicKey.toBase58(),
-                  a.pubkey === publicKey.toBase58()
-                )
-              ),
-              filter(({ pubkey }) => pubkey === publicKey.toBase58()),
-              map(({ accountInfo }) => accountInfo),
-              startWith(accountInfo)
-            )
+            this._solanaRpcSocketService
+              .onAccountChange(publicKey.toBase58())
+              .pipe(startWith(accountInfo))
           )
         );
     })
@@ -109,6 +99,7 @@ export class AppComponent implements OnInit {
   readonly walletAccountLamports$ = this.walletAccount$.pipe(
     map((accountInfo) => (accountInfo ? accountInfo.lamports : null))
   );
+  readonly rpcConnected$ = this._solanaRpcSocketService.connected$;
 
   constructor(
     private readonly _walletStore: WalletStore,
@@ -119,18 +110,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this._walletStore.setAdapters([new PhantomWalletAdapter()]);
-
-    this._walletStore.publicKey$
-      .pipe(
-        switchMap((publicKey) => {
-          return publicKey
-            ? this._solanaRpcSocketService.accountSubscribe(
-                publicKey.toBase58()
-              )
-            : of(null);
-        })
-      )
-      .subscribe();
   }
 
   onConnect() {
@@ -152,12 +131,6 @@ export class AppComponent implements OnInit {
         toPubkey: Keypair.generate().publicKey,
         lamports: 1,
       })
-      .subscribe();
-  }
-
-  onAccountUnsubscribe(walletPublicKey: PublicKey) {
-    this._solanaRpcSocketService
-      .accountUnsubscribe(walletPublicKey.toBase58())
       .subscribe();
   }
 }
--- apps/web-client/src/app/solana-rpc-socket.service.ts
+++ apps/web-client/src/app/solana-rpc-socket.service.ts
@@ -1,117 +1,125 @@
 import { Injectable } from '@angular/core';
-import {
-  concatMap,
-  filter,
-  fromEvent,
-  map,
-  Observable,
-  of,
-  share,
-  take,
-  tap,
-} from 'rxjs';
+import { AccountInfo } from '@solana/web3.js';
+import { BehaviorSubject, filter, map, Observable, Subject } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
 import { v4 as uuid } from 'uuid';
 import { environment } from '../environments/environment';
 
+interface RpcResultNotification {
+  jsonrpc: string;
+  id: string;
+  result: number;
+}
+
+interface Context {
+  slot: number;
+}
+
+interface RpcResponseAndContext<T> {
+  context: Context;
+  value: T;
+}
+
+interface RpcAccountParamsNotification {
+  result: RpcResponseAndContext<AccountInfo<Buffer>>;
+  subscription: number;
+}
+
+interface RpcNotification<T> {
+  jsonrpc: string;
+  method: string;
+  params: T;
+}
+
+type RpcMessage =
+  | RpcResultNotification
+  | RpcNotification<null>
+  | RpcNotification<RpcAccountParamsNotification>;
+
+const PING_DELAY_MS = 5_000;
+
 @Injectable({ providedIn: 'root' })
 export class SolanaRpcSocketService {
-  private readonly _ws = new WebSocket(environment.rpcWebsocket);
-  accountChange$ = fromEvent<MessageEvent>(this._ws, 'message').pipe(
-    share({
-      resetOnRefCountZero: false,
-    }),
-    map(({ data }) => JSON.parse(data)),
-    filter((message) => message.method === 'accountNotification'),
-    map((message) => ({
-      pubkey: this.accountLookup.get(message.params.subscription),
-      accountInfo: message.params.result.value,
-    }))
-  );
-  accountLookup = new Map<string, string>();
-  subscriptionLookup = new Map<string, string>();
+  private readonly _connected = new BehaviorSubject(false);
+  readonly connected$ = this._connected.asObservable();
+  messageSubject = new Subject();
+  private readonly _subject = webSocket<RpcMessage>({
+    url: environment.rpcWebsocket,
+    openObserver: {
+      next: (event) => {
+        console.log('connection open', event);
 
-  accountSubscribe(accountId: string) {
-    const correlationId = uuid();
-
-    return fromEvent(this._ws, 'open').pipe(
-      concatMap(() => {
-        this._ws.send(
-          JSON.stringify([
-            {
+        setInterval(
+          () =>
+            this._subject.next({
               jsonrpc: '2.0',
-              id: correlationId,
-              method: 'accountSubscribe',
-              params: [
-                accountId,
-                {
-                  encoding: 'base64',
-                  commitment: 'finalized',
-                },
-              ],
-            },
-          ])
+              method: 'ping',
+              params: null,
+            }),
+          PING_DELAY_MS
         );
 
-        return fromEvent<MessageEvent>(this._ws, 'message').pipe(
-          filter(({ data }) => {
-            const parsedData = JSON.parse(data);
+        this._connected.next(true);
+      },
+    },
+    closingObserver: {
+      next: (event) => {
+        console.log('closing connection', event);
+      },
+    },
+    closeObserver: {
+      next: (event) => {
+        console.log('connection closed', event);
+        this._connected.next(false);
+      },
+    },
+  });
 
-            if (!Array.isArray(parsedData) || parsedData.length === 0) {
-              return false;
-            }
-
-            return parsedData[0].id === correlationId;
-          }),
-          take(1),
-          map(({ data }) => JSON.parse(data)[0].result),
-          tap((subscriptionId) => {
-            this.accountLookup.set(subscriptionId, accountId);
-            this.subscriptionLookup.set(accountId, subscriptionId);
-          })
-        );
-      })
-    );
-  }
-
-  accountUnsubscribe(accountId: string) {
+  onAccountChange(accountId: string): Observable<AccountInfo<Buffer>> {
     const correlationId = uuid();
+    let subscriptionId: number;
 
-    const open$: Observable<unknown> =
-      this._ws.OPEN === this._ws.readyState
-        ? of(null)
-        : fromEvent(this._ws, 'open');
-
-    return open$.pipe(
-      concatMap(() => {
-        this._ws.send(
-          JSON.stringify([
+    return this._subject
+      .multiplex(
+        () => ({
+          jsonrpc: '2.0',
+          id: correlationId,
+          method: 'accountSubscribe',
+          params: [
+            accountId,
             {
-              jsonrpc: '2.0',
-              id: correlationId,
-              method: 'accountUnsubscribe',
-              params: [this.subscriptionLookup.get(accountId)],
+              encoding: 'base64',
+              commitment: 'finalized',
             },
-          ])
-        );
+          ],
+        }),
+        () => ({
+          jsonrpc: '2.0',
+          id: uuid(),
+          method: 'accountUnsubscribe',
+          params: [subscriptionId],
+        }),
+        (message: RpcMessage) => {
+          console.log(message);
 
-        return fromEvent<MessageEvent>(this._ws, 'message').pipe(
-          filter(({ data }) => {
-            const parsedData = JSON.parse(data);
+          if ('id' in message && message.id === correlationId) {
+            subscriptionId = message.result;
+          }
 
-            if (!Array.isArray(parsedData) || parsedData.length === 0) {
-              return false;
-            }
-
-            return parsedData[0].id === correlationId;
-          }),
-          take(1),
-          map(({ data }) => JSON.parse(data)[0].result),
-          tap((subscriptionId) => {
-            this.accountLookup.delete(accountId);
-            this.subscriptionLookup.delete(subscriptionId);
-          })
-        );
-      })
-    );
+          return (
+            'method' in message &&
+            message.method === 'accountNotification' &&
+            message.params !== null &&
+            message.params.subscription === subscriptionId
+          );
+        }
+      )
+      .pipe(
+        filter(
+          (message): message is RpcNotification<RpcAccountParamsNotification> =>
+            'method' in message
+        ),
+        map((message) => message.params.result.value)
+      );
   }
 }
